9eec4442985cfafe95b0a1d2865b5d9f0e8fbd7b
NoSoyAdrian <54293018+NoSoyAdrian@users.noreply.github.com>
2022-04-04 15:59:00 -0500

    Enemy (4)
    Disparo con sonido, conteo de enemigos y creación del gameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public static GameController Instance;
+    private int playerScore;
+
+    private void Awake() {
+        Instance = this;
+    }
+    public void OnDie(GameObject deadObject, int score = 0 ) {
+        playerScore += score;
+        Debug.LogFormat("GameController: the object {0} has died Adding score {1}, total: {2}", deadObject.name, score, playerScore);
+    }
+}
