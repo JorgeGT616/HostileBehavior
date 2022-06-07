e032db7e78bf823bafcf76ad6204ed8abbde00cc
NoSoyAdrian <54293018+NoSoyAdrian@users.noreply.github.com>
2022-06-07 16:36:31 -0500

    Sonidos y botón de menú
    sonido de game over, sonido de destrucción de meteoritos, botón de regreso al menú

--- Assets/Scripts/Salir.cs
+++ Assets/Scripts/Salir.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Salir : MonoBehaviour
 {
-    public void Exit(){
-        Application.Quit();
+    public void Jugar(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+    
 }
