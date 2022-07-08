3be2b8dac472cece5e20bf8b5f6c849d073bfd8e
Alejandro <alejandroup170@gmail.com>
2022-07-07 19:54:35 -0500

    Added Explosions

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class DestroyObject : MonoBehaviour {
 
     public float destroyTime;
-    
+
+    public GameObject ExplosionFX;
+
     public void DoDestroy(){
+        GameObject exp = Instantiate(ExplosionFX);
+        exp.transform.position = gameObject.transform.position;
         Destroy(gameObject, destroyTime);
     }
 }
